96da6bf91d039017c6e5903654bb34227427e286
f104a <f104a@f104a.io>
2024-10-31 14:37:42 +0100

    [setup wizard]: redesign welcome and finish activities
    Change-Id: I8b8c5911b4f92916d090bb5172376ddd187958d1

--- src/org/lineageos/setupwizard/WelcomeActivity.java
+++ src/org/lineageos/setupwizard/WelcomeActivity.java
@@ -13,6 +13,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.graphics.Color;
+
 
 import com.google.android.setupcompat.template.FooterButtonStyleUtils;
 import com.google.android.setupcompat.util.SystemBarHelper;
@@ -28,11 +32,26 @@ public class WelcomeActivity extends SubBaseActivity {
     protected void onStartSubactivity() {
     }
 
+    private void setupWelcomeBars(){
+	Window window = getWindow();
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+	window.setFlags(
+              WindowManager.LayoutParams.FLAG_FULLSCREEN,
+              WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        onSetupStart();
+	onSetupStart();
         SystemBarHelper.setBackButtonVisible(getWindow(), false);
+	setupWelcomeBars();
         setNextText(R.string.start);
         Button startButton = findViewById(R.id.start);
         Button emergButton = findViewById(R.id.emerg_dialer);
